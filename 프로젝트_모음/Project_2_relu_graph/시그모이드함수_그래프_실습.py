20b311ba3c20d7f4cd47ae3515a9824d11ef55e9
innoinho <161711823+innoinho@users.noreply.github.com>
2024-08-14 14:58:48 +0900

    Add files via upload

---
+++ 프로젝트_모음/Project_2_relu_graph/시그모이드함수_그래프_실습.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid(x)
+
+plt.plot(x, y)
+plt.title('Sigmoid Function')
+plt.xlabel('x')
+plt.ylabel('sigmoid(x)')
+plt.grid(True)
+plt.show()
+
+
+
